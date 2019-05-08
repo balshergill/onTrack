c19216f9ca29dbb5e4ff90151739221cd14df44a
Northcoders12 <northcoders12@Northcoders12s-MacBook-Pro.local>
2019-05-08 15:12:23 +0100

    changes icons and styling

--- ontrack-app/components/Menu.js
+++ ontrack-app/components/Menu.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { View, StyleSheet } from 'react-native';
 import { Icon } from 'react-native-elements'
+import AntIcon from "react-native-vector-icons/AntDesign";
 
 const Menu = () => {
   return (
@@ -10,15 +11,17 @@ const Menu = () => {
           name='search'
           type='feather'
           color='#9A9A9A'
-          size='50'
+          size='40'
+          class='Search'
+          onPress={() => this.props.navigation.navigate("Home")}
         />
       </View>
       <View style={styles.icon}>
-        <Icon
-          name='list'
-          type='feather'
-          color='#9A9A9A'
-          size='60'
+      <AntIcon 
+        name="bars" 
+        color="#9a9a9a" 
+        size={50} 
+        // onPress={() => this.props.navigation.navigate("AR")}
         />
       </View>
       <View style={styles.icon}>
@@ -26,7 +29,8 @@ const Menu = () => {
           name='user'
           type='feather'
           color='#9A9A9A'
-          size='50'
+          size='40'
+          onPress={() => this.props.navigation.navigate("User")}
         />
       </View>
     </View>
@@ -35,7 +39,7 @@ const Menu = () => {
 
 const styles = StyleSheet.create({
   Menu: {
-    flex: 0.9,
+    flex: 0.75,
     flexDirection: 'row',
     backgroundColor: 'rgba(0, 255, 0, 0.3)',
     backgroundColor: '#FFF',
@@ -47,8 +51,8 @@ const styles = StyleSheet.create({
   icon: {
     flexDirection: 'row',
     flex: 1,
-    marginLeft: '9%',
-    marginTop: '1.5%',
+    marginLeft: '10%',
+    marginTop: '3%',
   },
 });
 
