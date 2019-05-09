13d8d31c4d80140efa66cc46c3cb7836b3ad17d6
Northcoders12 <northcoders12@Northcoders12s-MacBook-Pro.local>
2019-05-09 12:10:08 +0100

    adds payment button component

---
+++ ontrack-app/components/PaymentButton.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Text, StyleSheet, TouchableOpacity, Linking } from 'react-native';
+
+const PaymentButton = () => {
+  return (
+    <TouchableOpacity style={styles.PaymentButton} onPress={() => Linking.openURL('http://ojp.nationalrail.co.uk/service/timesandfares/LDS/BUY/tomorrow/1415/dep')}>
+      <Text>Purchase ticket</Text>
+    </TouchableOpacity>
+  )
+}
+
+const styles = StyleSheet.create({
+    PaymentButton: {
+      justifyContent: 'center',
+      width: '75%',
+      height: '5%',
+      alignItems: 'center',
+      backgroundColor: '#FFF',
+      borderRadius: '3',
+    },
+  });
+
+export default PaymentButton
