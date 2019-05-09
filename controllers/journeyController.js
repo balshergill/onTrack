61ac9487cfeeb5c8f644b9ef036fb10a08b8bdfe
Tom Abbott <tommcmin@gmail.com>
2019-05-09 12:56:47 +0100

    adds crs and station name matching

--- controllers/journeyController.js
+++ controllers/journeyController.js
@@ -4,6 +4,7 @@ const {
   fetchLiveNext
 } = require('../models/liveModels');
 
+
 exports.getJourney = (req, res, next) => {
   fetchLiveJourney(req.params, req.query, (err, data) => {
     if (err) throw err;
--- models/liveModels.js
+++ models/liveModels.js
@@ -1,4 +1,28 @@
 const { rail } = require('../keys');
+const stations = require('../station_codes.json');
+
+// console.log(stations[0]['CRS Code'] === 'LDS');
+
+const stationInfo = (startCrs, endCrs) => {
+  let result = {
+    start: null,
+    end: null
+  };
+  let i = 0;
+  while (!result.start || !result.end) {
+    let stCrs = stations[i]['CRS Code'];
+    if (stCrs === startCrs) {
+      result.start = stations[i];
+    }
+    if (stCrs === endCrs) {
+      result.end = stations[i];
+    }
+    i++;
+  }
+  return result;
+};
+
+console.log(stationInfo('LDS', 'YRK'));
 
 exports.fetchLiveJourney = (params, query, callback) => {
   const destination = query.to;
