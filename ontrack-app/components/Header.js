2b8c20d80afd4b15bdad077ef98c73befdb576f0
Northcoders12 <northcoders12@Northcoders12s-MacBook-Pro.local>
2019-05-09 10:17:18 +0100

    adds some styling to Header component

--- ontrack-app/components/Header.js
+++ ontrack-app/components/Header.js
@@ -13,13 +13,13 @@ const styles = StyleSheet.create({
   Header: {
     flex: 1,
     flexDirection: 'column',
-    backgroundColor: 'rgba(0, 0, 255, 0.3)',
     justifyContent: 'center',
     width: '100%',
     alignItems: 'center',
     backgroundColor: '#FFF',
     borderBottomWidth: 3,
     borderBottomColor: '#0996F6',
+    marginBottom: 0,
   },
 });
 
