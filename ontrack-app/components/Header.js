27f405193db228eb094e6dd89c80eeab44985827
Northcoders12 <northcoders12@Northcoders12s-MacBook-Pro.local>
2019-05-09 16:51:14 +0100

    add 'logo' to the header of the app

--- ontrack-app/components/Header.js
+++ ontrack-app/components/Header.js
@@ -4,7 +4,7 @@ import { Text, View, StyleSheet } from 'react-native';
 const Header = () => {
   return (
     <View style={styles.Header}>
-      <Text>Header</Text>
+      <Text style={styles.Logo}>onTrack</Text>
     </View>
   )
 }
@@ -21,6 +21,12 @@ const styles = StyleSheet.create({
     borderBottomColor: '#0996F6',
     marginBottom: 0,
   },
+  Logo: {
+    marginTop: '10%',
+    fontSize: '35em',
+    textAlign: 'left',
+    color: '#9A9A9A',
+  }
 });
 
 export default Header;
