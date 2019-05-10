173b8bae3862e6947a245677a93ae8ef2ea90de6
Northcoders12 <northcoders12@Northcoders12s-MacBook-Pro.local>
2019-05-10 15:48:43 +0100

    create placeholders for info for service cards and styles them

--- ontrack-app/components/ServiceCard.js
+++ ontrack-app/components/ServiceCard.js
@@ -1,22 +1,114 @@
-import React from 'react'
-import { Text, View, StyleSheet } from 'react-native';
+import React, { Component } from 'react';
+import PaymentButton from './PaymentButton.js';
+import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
+import { Icon } from 'react-native-elements';
 
-const ServiceCard = () => {
-  return (
-    <View style={styles.ServiceCard}>
-      <Text>Service Card</Text>
-    </View>
-  )
+class ServiceCard extends Component {
+  state = {
+    openCard: false,
+  }
+  render () {
+    return (
+      <TouchableOpacity style={styles.ServiceCard} onPress={() => this.handleClick()}>
+        <View style={styles.origin}>
+          <Text>Origin Station (CRS)</Text>
+        </View>
+        <View style={this.state.openCard === false ? styles.destination : styles.clickedDestination}>
+          <Text>Destination (CRS)</Text>
+        </View>
+        <View style={styles.time}>
+          <Text>Time of Service</Text>
+        </View>
+        <View style={this.state.openCard === false ? styles.icon : styles.clickedIcon}>
+        <Icon
+          name={this.props.reliability === 'yes' ? 'check-circle' : this.props.reliability === 'no' ? 'times-circle' : 'minus-circle'}
+          type='font-awesome'
+          color={this.props.reliability === 'yes' ? 'rgb(0, 200, 0)' : this.props.reliability === 'no' ? 'red' : 'orange'}
+          size= '50'
+        />
+        </View>
+        {this.state.openCard === true ? 
+          <View>
+            <PaymentButton style={styles.Payment}/>
+            <View style={styles.chevron}>
+              <Icon
+                name='chevron-left'
+                type='font-awesome'
+                color='#9A9A9A'
+                size= '40'
+              />
+            </View>
+          </View> 
+        : null }
+      </TouchableOpacity>
+    )
+  }
+  handleClick = () => {
+    if (this.state.openCard === false) {
+      this.setState({
+        openCard: true
+      })
+    } else {
+      this.setState({
+        openCard: false
+      })
+    }
+    console.log(this.state.openCard)
+  }
 }
 
 const styles = StyleSheet.create({
-    Main: {
-      flexDirection: 'column',
-      textAlign: 'center',
-      alignSelf: 'stretch',
-      alignItems: 'center',
-    },
-  });
+  ServiceCard: {
+    flex: 1,
+    backgroundColor: '#EFEFEF',
+    width: 350,
+    height: 100,
+    borderBottomWidth: 2,
+    borderBottomColor: '#0996F6',
+    marginTop: 30,
+    shadowColor: '#000',
+    shadowOffset: { width: 4, height: 5 },
+    shadowOpacity: 0.2,
+    shadowRadius: 2,
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  origin: {
+    position: 'absolute',
+    top: 10,
+    left: 10,
+  },
+  destination: {
+    position: 'absolute',
+    top: 10,
+    right: 10,
+  },
+  clickedDestination: {
+    position: 'absolute',
+    top: 10,
+    right: 100,
+  },
+  time: {
+    position: 'absolute',
+    bottom: 25,
+    left: 10,
+  },
+  icon: {
+    position: 'absolute',
+    bottom: 10,
+    right: 20,
+  },
+  clickedIcon: {
+    position: 'absolute',
+    bottom: 10,
+    right: 100,
+  },
+  chevron: {
+    position: 'absolute',
+    bottom: 26,
+    right: 11,
+  }
+});
   
 
 export default ServiceCard;
