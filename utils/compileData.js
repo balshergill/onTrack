098bafba19eebc15f3670f95fcce26763d7ebba5
balshergill <baldipkshergill@gmail.com>
2019-05-10 11:11:32 +0100

    fixed issue with multiple seeding

--- utils/compileData.js
+++ utils/compileData.js
@@ -1,7 +1,7 @@
-const fs = require('fs');
-const path = require('path');
-const directoryPath = path.join(__dirname, '../') + 'db/data/raw-data';
-// console.log(directoryPath);
+const fs = require("fs");
+const path = require("path");
+const directoryPath = path.join(__dirname, "../") + "db/data/raw-data";
+console.log(directoryPath);
 
 exports.compileData = () => {
   return new Promise(function(resolve, reject) {
@@ -9,11 +9,12 @@ exports.compileData = () => {
       const data = [];
       if (err) return console.log(err);
       else {
+        console.log(files.length);
         files.forEach(function(file) {
-          let contents = fs.readFileSync(`${directoryPath}/${file}`, 'utf8');
+          let contents = fs.readFileSync(`${directoryPath}/${file}`, "utf8");
           const dataToEdit = JSON.parse(contents);
+          let wantedData;
           for (let i = 0; i < dataToEdit.locations.length; i++) {
-            let wantedData;
             if (i < dataToEdit.locations.length - 2) {
               wantedData = {
                 service_id: dataToEdit.serviceUid,
@@ -57,12 +58,11 @@ exports.compileData = () => {
                 cancelled_reason: dataToEdit.locations[i].cancelReasonLongText
               };
             }
-            data.push(wantedData);
           }
+          data.push(wantedData);
         });
       }
       newData = JSON.stringify(data);
-      console.log('compiled');
       resolve(data);
     });
   });
