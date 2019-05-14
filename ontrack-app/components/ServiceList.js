60c5d595209969d80998c163f42c8a8a8a4d30fb
Northcoders12 <northcoders12@Northcoders12s-MacBook-Pro.local>
2019-05-14 16:56:02 +0100

    make start on duplicating and populating service Cards

--- ontrack-app/components/ServiceList.js
+++ ontrack-app/components/ServiceList.js
@@ -1,13 +1,42 @@
 import React, { Component } from 'react';
 import ServiceCard from './ServiceCard';
-import { ScrollView } from 'react-native';
+import { ScrollView, View, Text, StyleSheet } from 'react-native';
 
-export default class ServiceList extends Component {
-  render() {
-    return (
+export default ServiceList = props => {
+  if (props.trainData["1"] !== undefined) { 
+    objData = JSON.parse(props.trainData)
+    console.log(objData)
+    serviceArr = objData["liveResult"]["trainServices"]
+    serviceArr.map (service => {
+      const destination = service["subsequentCallingPoints"][service["subsequentCallingPoints"].length -1]["locationName"]
+      const destCRS = service["subsequentCallingPoints"][service["subsequentCallingPoints"].length -1]["crs"]
+      // console.log(destination)
+      // console.log(destCRS)
+      return (
       <ScrollView showsVerticalScrollIndicator={false}>
-        <ServiceCard reliability='maybe'/>
+        <ServiceCard reliability='maybe' trainData={props.trainData} trainData={objData} destination={destination} destCRS={destCRS}/>
       </ScrollView>
-    );
+      )
+    })
   }
+  return (
+    <View>
+    {props.trainData === undefined || Object.keys(props.trainData).length < 1
+    ? <Text style={styles.placeholderText}>Please specify the journey you want to take.</Text> 
+    : <ScrollView showsVerticalScrollIndicator={false}>
+        <ServiceCard reliability='maybe' trainData={props.trainData} />
+      </ScrollView>}
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  Main: {
+    flex: 6,
+    flexDirection: 'column',
+    textAlign: 'center',
+    alignSelf: 'stretch',
+    alignItems: 'center',
+  },
+});
+
