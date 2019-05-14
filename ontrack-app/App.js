f68fb0c43268c4d517b68d1f9e963487f2b16ac6
andreiFC <cislariuandrei@Cislarius-MacBook-Pro.local>
2019-05-14 12:49:41 +0100

    Add loading spinner

--- ontrack-app/App.js
+++ ontrack-app/App.js
@@ -2,11 +2,29 @@ import React, { Component } from 'react';
 import Header from './components/Header.js';
 import Body from './components/Body.js';
 import { StyleSheet, View } from 'react-native';
+import Spinner from 'react-native-loading-spinner-overlay';
 
 export default class App extends Component {
+  state = {
+    spinner: true
+  };
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        spinner: !this.state.spinner
+      });
+    }, 3000);
+  }
+  
   render() {
     return (
       <View style={styles.container}>
+        <Spinner
+          visible={this.state.spinner}
+          color={'#0996F6'}
+          animation={'slide'}
+        />
         <Header />
         <Body />
       </View>
