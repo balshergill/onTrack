69c2fb132fe021b0061c43c713c2418fd4b421a0
Northcoders12 <northcoders12@Northcoders12s-MacBook-Pro.local>
2019-05-15 12:31:16 +0100

    create map that duplicates service cards

--- ontrack-app/components/ServiceList.js
+++ ontrack-app/components/ServiceList.js
@@ -1,33 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ServiceCard from './ServiceCard';
 import { ScrollView, View, Text, StyleSheet } from 'react-native';
+import { exactSearch } from '../search';
 
 export default ServiceList = props => {
-  if (props.trainData["1"] !== undefined) { 
-    objData = JSON.parse(props.trainData)
-    console.log(objData)
-    serviceArr = objData["liveResult"]["trainServices"]
-    serviceArr.map (service => {
-      const destination = service["subsequentCallingPoints"][service["subsequentCallingPoints"].length -1]["locationName"]
-      const destCRS = service["subsequentCallingPoints"][service["subsequentCallingPoints"].length -1]["crs"]
-      // console.log(destination)
-      // console.log(destCRS)
-      return (
-      <ScrollView showsVerticalScrollIndicator={false}>
-        <ServiceCard reliability='maybe' trainData={props.trainData} trainData={objData} destination={destination} destCRS={destCRS}/>
-      </ScrollView>
-      )
-    })
+  if (typeof JSON.parse(props.trainData) !== 'object') {
+    return(
+      <View>
+        <Text style={styles.placeholderText}>Please specify the journey you want to take.</Text>
+      </View>
+    )
   }
+  objData = JSON.parse(props.trainData)
+    const fromStation = exactSearch(props.origin)
+    serviceArr = objData["liveResult"]["trainServices"]
   return (
-    <View>
-    {props.trainData === undefined || Object.keys(props.trainData).length < 1
-    ? <Text style={styles.placeholderText}>Please specify the journey you want to take.</Text> 
-    : <ScrollView showsVerticalScrollIndicator={false}>
-        <ServiceCard reliability='maybe' trainData={props.trainData} />
-      </ScrollView>}
-    </View>
-  );
+  <ScrollView showsVerticalScrollIndicator={false}>
+    {serviceArr.map (service => {
+      const index = service["subsequentCallingPoints"].findIndex(
+        station => station.crs === props.destination
+      )
+    return (
+        <ServiceCard reliability='maybe' service={service} origin={fromStation} destination={service['subsequentCallingPoints'][index]}/>
+    )})}
+  </ScrollView>
+  )
 }
 
 const styles = StyleSheet.create({
