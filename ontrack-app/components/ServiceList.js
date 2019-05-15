a785788b219a6190186c035acda2a13a73882e4f
Northcoders12 <northcoders12@Northcoders12s-MacBook-Pro.local>
2019-05-15 16:54:31 +0100

    allow search to use station names

--- ontrack-app/components/ServiceList.js
+++ ontrack-app/components/ServiceList.js
@@ -4,6 +4,9 @@ import { ScrollView, View, Text, StyleSheet } from 'react-native';
 import { exactSearch } from '../search';
 
 export default ServiceList = props => {
+  const OCRS = exactSearch(props.origin)['CRS Code']
+  const DCRS = exactSearch(props.destination)['CRS Code']
+  console.log(props)
   if (typeof JSON.parse(props.trainData) !== 'object') {
     return(
       <View>
@@ -12,16 +15,18 @@ export default ServiceList = props => {
     )
   }
   objData = JSON.parse(props.trainData)
-    const fromStation = exactSearch(props.origin)
+    const fromStation = exactSearch(OCRS)
     serviceArr = objData["liveResult"]["trainServices"]
+    resultArr = objData["result"]
   return (
   <ScrollView showsVerticalScrollIndicator={false}>
-    {serviceArr.map (service => {
+    {serviceArr.map ((service, i) => {
+      const depMinsLate = resultArr[i]["rows"][0]['dep_minutes_late']
       const index = service["subsequentCallingPoints"].findIndex(
-        station => station.crs === props.destination
+        station => station.crs === DCRS
       )
     return (
-        <ServiceCard reliability='maybe' service={service} origin={fromStation} destination={service['subsequentCallingPoints'][index]}/>
+        <ServiceCard reliability='maybe' service={service} origin={fromStation} destination={service['subsequentCallingPoints'][index]} mins={depMinsLate}/>
     )})}
   </ScrollView>
   )
