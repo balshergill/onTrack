b49ecbd2011f35777c2e27225f6dceba5f8510da
tabbeyy <47356117+tabbeyy@users.noreply.github.com>
2019-05-16 10:04:21 +0100

    Delete userController.js

--- controllers/userController.js
+++
@@ -1,8 +0,0 @@
-const { fetchUsers } = require("../models/userModels")
-
-exports.getUser = (req, res, next) => {
-    fetchUsers(req.params, req.query), (err, user) => {
-        if (err) throw err;
-        res.status(200).json(user);
-      };
-}
