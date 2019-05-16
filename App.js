6d65eb87b3d142ed6ec59323e810605f88d56ce8
Tom Abbott <tommcmin@gmail.com>
2019-05-16 10:07:05 +0100

    adds user login

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
-const express = require("express");
-const cors = require("cors");
-const journeyRouter = require("./routes/journeyRouter");
-
+const express = require('express');
+const cors = require('cors');
+const journeyRouter = require('./routes/journeyRouter');
+const userRouter = require('./routes/userRouter');
 const app = express();
+
+const bodyParser = require('body-parser');
+app.use(bodyParser());
 
 app.use(cors());
 
-app.use("/journey", journeyRouter);
+app.use('/journey', journeyRouter);
+app.use('/user', userRouter);
 
 app.use(express.json());
 
--- spec/App.spec.js
+++ spec/App.spec.js
@@ -5,21 +5,51 @@ const App = require('../App');
 const request = supertest(App);
 const db = require('../db/connection');
 
-describe('/journey', () => {
+// describe('/journey', () => {
+//   after(() => {
+//     db.destroy();
+//   });
+//   it('GET status 200 and object of objects', () => {
+//     return request
+//       .get('/journey?from=YRK&to=MIA')
+//       .expect(200)
+//       .then(({ body }) => {
+//         expect(body).to.be.an('Object');
+//         expect(body).to.contain.keys('liveResult', 'result');
+//         expect(body.result).to.be.an('Array');
+//         expect(body.liveResult).to.be.an('Object');
+//         expect(body.result[0].rows[0]['dep minutes late']).to.not.equal(null);
+//         // console.log(body.result[2], '<<< body');
+//       });
+//   });
+// });
+
+describe('/user', () => {
   after(() => {
     db.destroy();
   });
-  it('GET status 200 and object of objects', () => {
+  it('POST valid user details returns true', () => {
     return request
-      .get('/journey?from=YRK&to=MIA')
+      .post('/user/login')
+      .send({
+        username: 'test1',
+        password: 'password'
+      })
       .expect(200)
       .then(({ body }) => {
-        expect(body).to.be.an('Object');
-        expect(body).to.contain.keys('liveResult', 'result');
-        expect(body.result).to.be.an('Array');
-        expect(body.liveResult).to.be.an('Object');
-        expect(body.result[0].rows[0]['dep minutes late']).to.not.equal(null);
-        // console.log(body.result[2], '<<< body');
+        expect(body.verified).to.equal(true);
+      });
+  });
+  it('POST invalid user details returns false', () => {
+    return request
+      .post('/user/login')
+      .send({
+        username: 'bad',
+        password: 'details'
+      })
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.verified).to.equal(false);
       });
   });
 });
